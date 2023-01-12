df99b11a2d672bca555701b6ade7c14e1e2986d7
lucasjotap <leiathemosthot@gmail.com>
2023-01-12 13:23:11 -0300

    New commit

--- pythonic_carddeck.py
+++ pythonic_carddeck.py
@@ -1,14 +1,15 @@
 import collections
+from random import choice
 
 Card = collections.namedtuple('Card', ['rank', 'suit'])
 
 class FrenchDeck:
-    ranks = [str(n) for n in range(2, 11)] + list('JOKA')
-    suits = 'spades diamonds clubs and hearts'.split()
+    ranks = [str(n) for n in range(2, 11)] + list('JQKA')
+    suits = 'spades diamonds clubs hearts'.split()
 
     def __init__(self) -> None:
-        self._cards = [[Card(rank, suit) for suit in self.suits
-                                            for rank in self.ranks]]
+        self._cards = [Card(rank, suit) for suit in self.suits
+                                        for rank in self.ranks]
 
     def __len__(self):
         return len(self._cards)
@@ -20,5 +21,7 @@ class FrenchDeck:
 beer_card = Card('7', 'diamonds')
 print(beer_card)
 
+deck = FrenchDeck()
+print(len(deck))
 
                                         
