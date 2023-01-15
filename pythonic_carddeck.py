677d762bc05a4de82ce4131ea2107ef7295accd9
lucasjotap <leiathemosthot@gmail.com>
2023-01-14 22:37:10 -0300

    New script

--- pythonic_carddeck.py
+++ pythonic_carddeck.py
@@ -18,10 +18,30 @@ class FrenchDeck:
         return self._cards[position]
 
 
-beer_card = Card('7', 'diamonds')
-print(beer_card)
+# beer_card = Card('7', 'diamonds')
+# print(beer_card)
 
 deck = FrenchDeck()
-print(len(deck))
+# print(len(deck))
 
-                                        
+# # for card in deck:
+# #     print(card)
+
+# print(Card ('Q', 'hearts') in deck)
+
+suit_values = dict(spades=3, hearts=2, diamonds=1, clubs=0)
+
+def spades_high(card):
+    rank_value = FrenchDeck.ranks.index(card.rank)
+    return rank_value * len(suit_values) + suit_values[card.suit]
+
+for card in sorted(deck, key=spades_high):
+    print(card)
+    if card[1] == 'diamonds':
+        print("Here's a diamond")
+    elif card[1] == 'hearts':
+        print("Here's a hearts")
+    elif card[1] == "spades":
+        print("Here's a spades")
+    else:
+        print("Here's a clubs")
