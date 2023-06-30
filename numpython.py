7f68d805c3b11be6efd773106f7714935ed08bc8
lucasjotap <leiathemosthot@gmail.com>
2023-06-29 21:16:57 -0300

    numpy

---
+++ numpython.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+# A 2-dimensional array of 2 x 3, composed of 4-byte integer elements: 
+x = np.array([[1, 2, 3], [4, 5, 6]], np.int32)
+print(type(x)) # <class 'numpy.ndarray'>
+print(x.shape) # (2, 3)
